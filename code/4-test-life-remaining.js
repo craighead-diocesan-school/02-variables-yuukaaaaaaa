86883f5092daf9615df4b2a6af68de6f7b2c084c
yuukaaaaaaa <sugiyayu93@craighead.school.nz>
2023-06-01 11:14:26 -0700

    completed tasks

--- code/4-test-life-remaining.js
+++ code/4-test-life-remaining.js
@@ -3,5 +3,7 @@
 // ####################################
 
 function lifeRemaining() {
-  
+  let age = prompt('How old are you?')
+  let result= 83.5 - age
+  alert("You've got " + result + ' years left in you.')
 }
